8b635392492a230db66487a6914bdf64a2d7bb60
kacperaugustyn <kacperaugustyn@o2.pl>
2023-03-12 13:00:37 +0100

    makefile 3, 4

--- zestaw2/zad4/main.c
+++ zestaw2/zad4/main.c
@@ -5,7 +5,7 @@ long long total_size = 0;
 
 
 int check_file(const char *fpath, const struct stat *sb, int typeflag){
-
+    
     if ( !S_ISDIR(sb->st_mode)) {
         total_size += sb->st_size;
         printf("%s %lld\n", fpath, sb->st_size);
@@ -25,7 +25,7 @@ int main(int argc, char* argv[]){
     int error = ftw(argv[1], check_file, 16);
 
     if (error==-1){
-        printf("Error while searching given directory!");
+        printf("Error while searching given directory!\n");
         return 0;
     }
 
